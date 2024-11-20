c881c62b46b46a39d656102d00a8eb43a9b3a802
rinsi-Gupta <131142131+rinsi-Gupta@users.noreply.github.com>
2024-11-20 21:01:21 +0530

    Update PlayerContext.jsx

--- spotify-clone/src/context/PlayerContext.jsx
+++ spotify-clone/src/context/PlayerContext.jsx
@@ -7,7 +7,7 @@ const PlayerContextProvider = (props) => {
     const audioRef = useRef(null);
     const seekBg = useRef(null);
     const seekBar = useRef(null);
-    const url = 'http://localhost:4000';
+    const url = 'https://spotify-uhre.onrender.com';
 
     const [songsData, setSongsData] = useState([]);
     const [albumsData, setAlbumsData] = useState([]);
